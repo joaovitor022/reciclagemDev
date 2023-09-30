f8d49a4dcf5d641fe6b8e437f6a6d366a35ee797
Joao Vitor <jhonvitor539@gmail.com>
2023-09-30 13:41:10 -0300

    fazendo umas modificações

--- js/front.js
+++ js/front.js
@@ -1,1 +1,9 @@
-$('main a')
+function mostrarEmpresa(){
+    document.querySelector('.usuario').style.display = 'none';
+    document.querySelector('.empresa').style.display = 'block';
+}
+
+function mostrarUsuario(){
+    document.querySelector('.empresa').style.display = 'none';
+    document.querySelector('.usuario').style.display = 'block';
+}
