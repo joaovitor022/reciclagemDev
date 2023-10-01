50bf3f4e000acf854e55852c6c3931a7f6f05e1a
Gabriel Moreno Serignolli <tayseri@hotmail.com>
2023-10-01 18:22:39 -0300

    Ajuste nos botões de login e registro da página e inicial e adição da animação de alerta de clique em todos os botões

--- js/front.js
+++ js/front.js
@@ -1,4 +1,4 @@
-
+//Funções dos botões do cadastro
 function mostrarEmpresa(){
     document.querySelector('.usuario').style.display = 'none';
     document.querySelector('.empresa').style.display = 'flex';
@@ -10,7 +10,6 @@ function mostrarEmpresa(){
     document.querySelector('.btn-usuario').style.color = '#ffffff';
     document.querySelector('.btn-usuario').style.backgroundColor = '#0db0b8';
 }
-
 function mostrarUsuario(){
     document.querySelector('.empresa').style.display = 'none';
     document.querySelector('.usuario').style.display = 'flex';
@@ -21,4 +20,34 @@ function mostrarUsuario(){
 
     document.querySelector('.btn-empresa').style.color = '#ffffff';
     document.querySelector('.btn-empresa').style.backgroundColor = '#0db0b8';
+}
+
+//Funções dos botões da página inicial
+document.querySelector('.login').addEventListener('mouseover', hoverLogin);
+document.querySelector('.cadastro').addEventListener('mouseover', hoverCadastro);
+document.querySelector('.login').addEventListener('mouseout', hoverLoginOut);
+document.querySelector('.cadastro').addEventListener('mouseout', hoverCadastroOut);
+function hoverLogin() {
+    document.querySelector('.login').style.backgroundColor = '#38b6ff';
+    document.querySelector('.login').style.color = '#ffffff';
+    document.querySelector('.cadastro').style.backgroundColor = '#ffffff';
+    document.querySelector('.cadastro').style.color = '#38b6ff';
+}
+function hoverCadastro() {
+    document.querySelector('.cadastro').style.backgroundColor = '#38b6ff';
+    document.querySelector('.cadastro').style.color = '#ffffff';
+    document.querySelector('.login').style.backgroundColor = '#ffffff';
+    document.querySelector('.login').style.color = '#38b6ff';
+}
+function hoverLoginOut() {
+    document.querySelector('.login').style.backgroundColor = '';
+    document.querySelector('.login').style.color = '';
+    document.querySelector('.cadastro').style.backgroundColor = '';
+    document.querySelector('.cadastro').style.color = '';
+}
+function hoverCadastroOut() {
+    document.querySelector('.cadastro').style.backgroundColor = '';
+    document.querySelector('.cadastro').style.color = '';
+    document.querySelector('.login').style.backgroundColor = '';
+    document.querySelector('.login').style.color = '';
 }
