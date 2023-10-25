ffc504da44390121ab15f620375298779ecf0d90
Joao Vitor <jhonvitor539@gmail.com>
2023-10-25 12:03:54 -0300

    fazendo um teste pra ver maven

--- src/main/java/br/com/reciclagemDev/servlet/CreateCarServlet.java
+++ src/main/java/br/com/reciclagemDev/servlet/CreateCarServlet.java
@@ -1,11 +1,12 @@
-package br.com.carstore.servlet;
+package br.com.reciclagemDev.servlet;
+
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/create-car")
 public class CreateCarServlet extends HttpServlet {
