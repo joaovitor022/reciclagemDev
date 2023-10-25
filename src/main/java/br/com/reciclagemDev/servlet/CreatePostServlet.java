a3a4299bb32f70e467946b94cba3d1ef7372cfeb
Joao Vitor <jhonvitor539@gmail.com>
2023-10-25 12:55:16 -0300

    fazendo teste pra maven

--- src/main/java/br/com/reciclagemDev/servlet/CreatePostServlet.java
+++ src/main/java/br/com/reciclagemDev/servlet/CreatePostServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/create-car")
+@WebServlet("/post")
 public class CreateCarServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
